24c1b0ec8caf3661ce26968f265ca73358a039b7
Chanchal Zoarder <bdchanchal@gmail.com>
2015-07-06 10:41:14 +0600

    Should be changed
    Problem in Show All Button

---
+++ CitizenIdDictionaryApp/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CitizenIdDictionaryApp
+{
+    partial class DictionryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.detailsTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchIDTextBox = new System.Windows.Forms.TextBox();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(130, 40);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(214, 20);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(371, 116);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Details";
+            // 
+            // detailsTextBox
+            // 
+            this.detailsTextBox.Location = new System.Drawing.Point(130, 76);
+            this.detailsTextBox.Multiline = true;
+            this.detailsTextBox.Name = "detailsTextBox";
+            this.detailsTextBox.Size = new System.Drawing.Size(214, 63);
+            this.detailsTextBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.detailsTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.idTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(47, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 201);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info Entry";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ID";
+            // 
+            // searchIDTextBox
+            // 
+            this.searchIDTextBox.Location = new System.Drawing.Point(177, 261);
+            this.searchIDTextBox.Name = "searchIDTextBox";
+            this.searchIDTextBox.Size = new System.Drawing.Size(214, 20);
+            this.searchIDTextBox.TabIndex = 4;
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(206, 304);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(114, 36);
+            this.showAllButton.TabIndex = 2;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(418, 259);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // DictionryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 420);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.searchIDTextBox);
+            this.Controls.Add(this.label3);
+            this.Name = "DictionryUI";
+            this.Text = "Dictionary UI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox detailsTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox searchIDTextBox;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ CitizenIdDictionaryApp/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CitizenIdDictionaryApp
+{
+    public partial class DictionryUI : Form
+    {
+        Dictionary<int,string> citizenDitionary = new Dictionary<int, string>();
+        public int id;
+        public string details;
+        public int searchId;
+        public int count = 0;
+        public DictionryUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (idTextBox.Text != "" && detailsTextBox.Text != "")
+            {
+                id = Convert.ToInt32(idTextBox.Text);
+                details = detailsTextBox.Text;
+
+                citizenDitionary.Add(id, details);
+
+                idTextBox.Text = null;
+                detailsTextBox.Text = null;
+                MessageBox.Show("Saved Successfully");
+                count++;
+            }
+            else
+            {
+                MessageBox.Show("Please Enter ID and Details.");
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (searchIDTextBox.Text != "" && count != 0)
+            {
+                searchId = Convert.ToInt32(searchIDTextBox.Text);
+
+                if (citizenDitionary.ContainsKey(searchId))
+                {
+                    MessageBox.Show(citizenDitionary[searchId]);
+                    searchIDTextBox.Text = null;
+                }
+                else
+                {
+                    MessageBox.Show("Not Found");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must save Information first and write search ID here");
+            }
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            if (count != 0)
+            {
+                foreach (KeyValuePair<int, string> a in citizenDitionary)
+                {
+                    MessageBox.Show(a.Key + " " + a.Value);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Information has been saved yet");
+            }
+        }
+
+    }
+}
